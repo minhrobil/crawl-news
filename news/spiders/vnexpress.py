a9fe34562e9081bec9ad98b5888e6faedb4e3e21
Minhrobil <nguyenquangminh20111997@gmail.com>
2019-05-22 09:38:46 +0700

    up

--- news/spiders/vnexpress.py
+++ news/spiders/vnexpress.py
@@ -9,9 +9,13 @@ class QuotesSpider(scrapy.Spider):
 
     def start_requests(self):
         to_time_stamp = int(time.time())
-        from_time_stamp = to_time_stamp - 2676960*12
-        url = 'https://vnexpress.net/category/day?cateid=1003231&fromdate='+str(from_time_stamp)+'&todate='+str(to_time_stamp)+'&allcate=1003231||'
-        yield Request(url, self.parse)
+        array_cateid = ['1001005','1001002','1003159','1002691','1002565','1001007']
+        duration_day = 86400
+        duration_month = duration_day*30
+        from_time_stamp = to_time_stamp - 86400
+        for x in array_cateid:
+            url = 'https://vnexpress.net/category/day?cateid='+x+'&fromdate='+str(from_time_stamp)+'&todate='+str(to_time_stamp)+'&allcate='+x+'||'
+            yield Request(url, self.parse)
 
     def parse(self, response):
         next_url = 'https://vnexpress.net'+response.css('a.next::attr(href)').get()
@@ -32,6 +36,7 @@ class QuotesSpider(scrapy.Spider):
         author =  response.css('p.author_mail strong::text').get()
         item = {}
         item.update({
+            "url":response.url,
             "title":title,
             "description":description,
             "content":content,
